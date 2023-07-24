9a5a861ca86128240a6c9f8940ae38f64e66cea8
Jane Palacios <saraipalaciosjane@gmail.com>
2023-07-24 01:22:02 -0400

    Binding y variables

--- app/src/main/java/com/example/diesiochpersistence/MainActivity.kt
+++ app/src/main/java/com/example/diesiochpersistence/MainActivity.kt
@@ -4,10 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.diesiochpersistence.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btGuardar.setOnClickListener {
+            val text = binding.edTexto.text.toString()
+            val entero= binding.edEntero.text.toString().toInt()
+            val decimal = binding.edDecimal.text.toString().toDouble()
+            val switch = binding.switch1.isChecked
+        }
     }
+
 }
